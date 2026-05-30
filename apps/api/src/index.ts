4da6c1c0fbeae862d9e89b921d6be9174bf22fd3
Andrew Timothy <andrewt0x0265@gmail.com>
2026-05-29 23:07:44 -0400

    comments on what else I can do here

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,22 @@
 import express from 'express';
+import type { Request, Response } from 'express';
 import cors from 'cors';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3301; //cast as number bc process.env.PORT is always a string when set
 
+// interface HealthResponse{
+// 	status: 'ok';
+// 	uptime: number;
+// }
+
 app.use(cors({ origin: ['http://localhost:3300'] }));
 app.use(express.json());
-app.get('/health', function (_req, res) {//_req signals non-use
+// app.get('/health', function (_req : Request, res: Response<HealthResponse>) {//_req signals non-use
+// 	res.json({ status: 'ok', uptime: process.uptime() });
+// });
+ //could also do what's below **
+ app.get('/health', (_req: Request, res: Response<{ status: string; uptime: number }>) => {
 	res.json({ status: 'ok', uptime: process.uptime() });
 });
 app.listen(PORT, function () {
